bb044e1a37fa89a9d1eb5dc225e564942686fdc3
frankcee <fra16027@byui.edu>
2019-03-19 10:22:42 -0700

    update

--- assignments/lesson-10/scripts/windchill.js
+++
@@ -1,7 +0,0 @@
-function calwindchill() {
-    let t = parseInt(document.getElementById('currentTemp').innerHTML);
-    let s = parseInt(document.getElementById('windspeed').innerHTML);
-    let windchill =35.74+0.6215*t-35.75*(Math.pow(s,0.16))+0.4275*t*(Math.pow(s,0.16));
-    document.getElementById("windchill").innerHTML = windchill;
-    }
-    
